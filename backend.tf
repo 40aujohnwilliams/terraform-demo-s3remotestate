51a873f52ff20376b0df6781c23eb60250b5c50c
John Williams <john@fortyau.com>
2017-10-21 17:52:20 -0500

    Store remote state for this project in the remote state bucket created by this project

--- backend.tf
+++ backend.tf
@@ -5,3 +5,11 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "sample-remote-state-terraform-demo"
+    key    = "sample-remote-state"
+    region = "us-east-1"
+  }
+}
